07e727279133a15804f3f0f7529046ec5efe4694
Tyler Menezes <tylermenezes@gmail.com>
2014-01-11 14:45:55 -0800

    Fix me
    Signed-off-by: Tyler Menezes <tylermenezes@gmail.com>

--- includes/WarRoom.php
+++ includes/WarRoom.php
@@ -3,4 +3,13 @@
 include_once(implode(DIRECTORY_SEPARATOR, [dirname(__FILE__), 'submodules', 'Jetpack', 'Jetpack', 'App.php']));
 
 class WarRoom extends \Jetpack\App {
+    public static function after()
+    {
+        if (!is_cli()) {
+            // User Login Check
+            if (\WarRoom\Models\User::is_logged_in()) {
+                static::$twig->addGlobal('me', \WarRoom\Models\User::me());
+            }
+        }
+    }
 }
