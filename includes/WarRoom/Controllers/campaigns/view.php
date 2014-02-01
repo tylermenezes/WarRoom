8892e13c4f7ca798796e75553d08676f587f122e
Tyler Menezes <tylermenezes@gmail.com>
2014-02-01 01:43:08 -0800

    Add live updating, sounds.

--- includes/WarRoom/Controllers/campaigns/view.php
+++ includes/WarRoom/Controllers/campaigns/view.php
@@ -30,9 +30,46 @@ class view
                         ->where('campaignID = ?', $id)
                         ->all();
 
-        $autorefresh = $this->request->get('autorefresh') !== null;
+        $sound = $this->request->get('sound') !== null;
 
-        echo \WarRoom::$twig->render('campaigns/view.html.twig', ['autorefresh' => $autorefresh, 'campaign' => $campaign, 'my_links' => $my_links, 'users' => Models\User::get_leaders($campaign)]);
+        echo \WarRoom::$twig->render('campaigns/view.html.twig', ['sound' => $sound, 'campaign' => $campaign, 'my_links' => $my_links, 'users' => Models\User::get_leaders($campaign)]);
+    }
+
+    public function get_liveleaders($id)
+    {
+        try {
+            $campaign = Models\Campaign::one($id);
+        } catch (\TinyDb\NoRecordException $ex) {
+            throw new \CuteControllers\HttpError(401);
+        }
+
+        $my_links = Models\Link::find()
+                               ->where('userID = ?', Models\User::me()->id)
+                               ->where('campaignID = ?', $id)
+                               ->all();
+
+        $users = Models\User::get_leaders($campaign);
+        $leaderboard = array_map(function($user) use($campaign) {
+                return [
+                    'id' => $user->id,
+                    'first_name' => $user->first_name,
+                    'last_name' => $user->last_name,
+                    'clicks' => $user->get_clicks($campaign)
+                ];
+        }, iterator_to_array($users));
+
+        $links = array_map(function($link) {
+                return [
+                    'id' => $link->id,
+                    'clicks' => $link->clicks,
+                    'source_info' => $link->source_info
+                ];
+        }, iterator_to_array($my_links));
+
+        return [
+            'leaderboard' => $leaderboard,
+            'links' => $links
+        ];
     }
 
     public function post_link($id)
